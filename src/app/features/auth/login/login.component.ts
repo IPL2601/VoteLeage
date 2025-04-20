5886aa6ca966a8f8dbe906aa82d6a381f5784c0f
IPL2601 <ivanpelora2601@gmail.com>
2025-04-20 16:42:49 +0200

    Spinner y mensaje informativo del login y register

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -7,6 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
@@ -20,11 +21,14 @@ import { AuthService } from '../../../core/services/auth.service';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     RouterModule
   ],
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  isLoading = false;
+  message: string | null = null;
 
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
@@ -44,15 +48,20 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-  
+      this.isLoading = true;
+      this.message = null;
+
       this.auth.login({ email, password }).subscribe({
         next: () => {
           console.log('Login exitoso');
-          this.router.navigate(['/home']); // Redirige al home, si ya tienes ruta
+          this.message = 'Se ha logueado correctamente'
+          setTimeout(() => {
+            this.router.navigate(['/home']);
+          }, 1500);
         },
         error: (err) => {
           console.error('Error en login:', err.message);
-          alert('Login incorrecto. Revisa tus datos.');
+          this.message = 'Login incorrecto. Revisa tus datos.';
         }
       });
     }
--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../../core/services/auth.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   standalone: true,
@@ -20,11 +22,14 @@ import { AuthService } from '../../../core/services/auth.service';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     RouterModule
   ],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
+  successMessage: string | null = null;
+  isLoading = false;
 
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
     this.registerForm = this.fb.group({
@@ -49,17 +54,31 @@ export class RegisterComponent {
   onSubmit() {
     if (this.registerForm.valid) {
       const { email, username, password } = this.registerForm.value;
+  
+      this.isLoading = true;
+      this.successMessage = null;
 
       this.auth.register({ email, username, password }).subscribe({
         next: () => {
           console.log('Registro exitoso');
-          this.router.navigate(['/home']);
+  
+          // Mensaje de confirmación
+          this.successMessage = '¡Registro exitoso! Serás redirigido al login en unos segundos...';
+          this.isLoading = false;
+
+          // Esperamos 1.5 segundos antes de redirigir
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 1500);
         },
         error: (err) => {
           console.error('Error en registro:', err.message);
-          alert('No se pudo registrar. Intenta con otros datos.');
+          // Mensaje de error
+          this.successMessage = 'No se pudo registrar. Intenta con otros datos.';
+          this.isLoading = false;
         }
       });
     }
   }
+  
 }
